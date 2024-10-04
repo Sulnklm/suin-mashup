80a13697207adca05c604234e882c937321aa7c4
Sulnklm <hyeinsuin@icloud.com>
2024-10-03 22:27:51 -0700

    navbar icon

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 
 function App() {
+  // Array for poster img.
   const poster = [
     "https://i.ebayimg.com/images/g/99cAAOSwLURe2uIm/s-l1200.jpg",
     "https://image.tmdb.org/t/p/original/hKHZhUbIyUAjcSrqJThFGYIR6kI.jpg",
@@ -17,8 +18,8 @@ function App() {
     <div>
     <Header />
       <main>
-          <div className="flex items-center justify-center overflow-hidden relative w-full aspect-[4.8/2] video">
-             <iframe className="object-cover absolute  left-0 w-full h-full border-0" src="https://www.youtube.com/embed/LEjhY15eCx0?si=Nbw-uIrLIKeUluyL&autoplay=1&mute=1&loop=1&playlist=LEjhY15eCx0" title="YouTube video player"></iframe>
+          <div className="flex items-center justify-center overflow-hidden relative w-full  aspect-[4.78/2] lg:aspect-[4.78/2] video">
+             <iframe className="object-cover absolute left-0 w-full h-full border-0 top-[-60px] translate-y-14" src="https://www.youtube.com/embed/LEjhY15eCx0?si=Nbw-uIrLIKeUluyL&controls=0&showinfo=0&autoplay=1&mute=1&loop=1&playlist=LEjhY15eCx0" title="YouTube video player"></iframe>
           </div>
           <Card name="blog 1" poster={poster[0]} />
           <Card name="blog 2" poster={poster[1]} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,12 +16,12 @@ function Header() {
         <Navbar.Brand href="#home"><img className="w-[8rem] lg:w-[10rem] lg:px-3 h-auto" src="./public/logo.png" alt="" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto text-xl lg:space-x-5">
+          <Nav className="m-4 lg:text-xl lg:space-x-5 space-y-5 lg:space-y-0 md:space-y-0">
             <NavDropdown title="Movies" id="basic-nav-dropdown">
-              <div className="lg:p-2" >
-                <NavDropdown.Item className="lg:pb-3" href="#action/3.1">Now Showing</NavDropdown.Item>
-                <NavDropdown.Item className="lg:pb-3" href="#action/3.2">Coming Soon</NavDropdown.Item>
-                <NavDropdown.Item className="" href="#action/3.3">Top Rated</NavDropdown.Item>
+              <div className="space-y-6 lg:m-5 lg:space-y-7 lg:text-xl">
+                <NavDropdown.Item href="#action/3.1">Now Showing</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Coming Soon</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Top Rated</NavDropdown.Item>
               </div>
               
             </NavDropdown>
@@ -29,6 +29,10 @@ function Header() {
             <Nav.Link href="#link">Events</Nav.Link>
           </Nav>
         </Navbar.Collapse>
+        <div className="hidden lg:block flex text-2xl space-x-[4rem]">
+          <FontAwesomeIcon  icon={faUser} className="cursor-pointer"></FontAwesomeIcon>
+          <FontAwesomeIcon icon={faCircleInfo} className="cursor-pointer"></FontAwesomeIcon>
+        </div>
       </Container>
     </Navbar>
     );
