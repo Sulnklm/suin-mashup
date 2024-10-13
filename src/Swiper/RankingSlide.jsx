a126981d1d84a8cb2ff67e225fef26506d812bcd
Sulnklm <hyeinsuin@icloud.com>
2024-10-12 23:19:43 -0700

    banner switch

--- src/Swiper/RankingSlide.jsx
+++ src/Swiper/RankingSlide.jsx
@@ -7,46 +7,47 @@ import 'swiper/css/pagination';
 import Card from "../components/Card";
 import NowShowingData from "../config/data.json";
 
+
 function Ranking() {
-  const [nowShowingSlider, setNowShowingSlider] = useState(false);
+  const [nowShowingSlider, setNowShowingSlider] = useState(true);
   const [comingSoonSlider, setComingSoonSlider] = useState(false);
 
-  const nowShowingListBtn = () => {
-    console.log("test");
-    setNowShowingSlider(true);
-  };
 
-  const comingSoonListBtn = () => {
-    console.log("test");
-    setComingSoonSlider(true);
-  };
 
   return (
     <div className="flex place-content-center">
     <div className="max-w-[20rem] md:max-w-[40rem] lg:max-w-[80rem] lg:m-20">
       <div className="movieChart_btn_wrap m-3">
         <div className="tapBtn_wrap inline-flex space-x-5">
-          <h3 className="font-work" id="btnNowShowing">
+          <h3 className="font-work">
             <button
-              className="no-underline text-black"
-              onClick={nowShowingListBtn}
+              className={nowShowingSlider ? 'text-black' : 'text-gray-400'}
+              onClick={() => {
+                setNowShowingSlider(true);
+                setComingSoonSlider(false);
+              }
+            }
             >
               Now Showing
             </button>
           </h3>
           <p className="secondaryGray text-lg flex align-items-center">|</p>
-          <h3 className="font-work" id="btnComingSoon">
-            <button
-              className="no-underline text-black"
-              onClick={comingSoonListBtn}
+          <h3 className="font-work">
+            <button  
+              className={comingSoonSlider ? 'text-black' : 'text-gray-400'}
+              onClick={() => {
+                setNowShowingSlider(false);
+                setComingSoonSlider(true);
+              }}
             >
               Coming Soon
             </button>
           </h3>
         </div>
-      </div>
-
+        {/* nowShowingSlider */}
+      {nowShowingSlider && (
       <div className="max-w-[100rem]"> 
+        {nowShowingSlider}
         <Swiper effect={'coverflow'}
         grabCursor={true}
         centeredSlides={true}
@@ -69,6 +70,41 @@ function Ranking() {
           ))}
         </Swiper>
       </div>
+      )}
+
+
+      {/* comingSoonSlider */}
+      {comingSoonSlider && (
+      <div className="max-w-[100rem]"> 
+        {comingSoonSlider}
+        <Swiper effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'4'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        loop={true}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        >
+          {NowShowingData.comingSoonMovie.map((movie, index) => (
+            <SwiperSlide  key={index}>
+              <Card movie={movie} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+)}
+      </div>
+
+
+      
+
     </div>
     </div>
   );
